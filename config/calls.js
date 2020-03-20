cb06ce638a3fd8cb1cf306d62c8987bcad80bf53
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-19 21:06:28 -0700

    added comments and pared down calls code

--- config/calls.js
+++ config/calls.js
@@ -3,6 +3,7 @@ const accountSid = 'AC7a88ff772388157d0ffe6319140b678b';
 const authToken = process.env.TWILIO_KEY;
 const client = require('twilio')(accountSid, authToken);
 
+// callTimeout made accessible in module scope to clearTimeout
 let callTimeout;
 
 const getUpcoming = () => {
@@ -25,11 +26,12 @@ const getUpcoming = () => {
     })
         .then(results => {
             if (results) {
-                let upcomingCall = Date.parse(`${results.Event.event_date.split('T')[0]}T${results.call_time}:00.000`);
-                let currentTime = Date.parse(new Date());
-                let callid = results.shortid;
-                console.log("shortid in getupcoming", callid);
-                let phoneNumber = results.Event.User.phoneNumber;
+                const upcomingCall = Date.parse(`${results.Event.event_date.split('T')[0]}T${results.call_time}:00.000`);
+                const currentTime = Date.parse(new Date());
+                const callid = results.shortid;
+                const phoneNumber = results.Event.User.phoneNumber;
+
+                // in case of a server interruption, proceed to make calls if it is within 15 minutes scheduled time
                 if (upcomingCall - currentTime > -900000) {
                     startTimer(upcomingCall, callid, phoneNumber);
                 };
@@ -43,12 +45,12 @@ const getUpcoming = () => {
 };
 
 const startTimer = (upcomingCall, callid, phoneNumber) => {
-    console.log("STARTING TIMER!!!\n\n")
-    console.log("shortid in startTimer", callid);
-    let currentTime = Date.parse(new Date());
-    let delta = upcomingCall - currentTime;
-    console.log('upcomingCall', upcomingCall, 'currentTime', currentTime);
-    console.log("DELTA", delta);
+    const currentTime = Date.parse(new Date());
+
+    // milliseconds between the current time and the upcoming call
+    const delta = upcomingCall - currentTime;
+
+    // at the end of the time out, use Twilio to make the call
     callTimeout = setTimeout(() => {
         client.calls
             .create({
@@ -58,15 +60,20 @@ const startTimer = (upcomingCall, callid, phoneNumber) => {
             })
             .then(call => console.log(call.sid))
             .catch(err => console.log(err));
+
+        // delete the db entry after call is made
         db.Call.destroy({
             where: {
                 shortid: callid
             }
         });
+
+        // get next call
         getUpcoming();
     }, delta);
 };
 
+// update timer function: clears timeout and starts new timer
 const updateTimer = (newCall, callid, phoneNumber) => {
     clearTimeout(callTimeout);
     startTimer(newCall, callid, phoneNumber);
